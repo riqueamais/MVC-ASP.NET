71a9d8be95545d267d9b1d444f3be559592e600e
unisanta <unisanta@M216-17>
2023-10-27 20:56:52 -0300

    Add project files.

---
+++ MVC/Controllers/SerasaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class SerasaController : Controller
+    {
+        private readonly List<Serasa> devendo = new List<Serasa>();
+
+        public SerasaController()
+        {
+            devendo.Add(new Serasa()
+            { 
+                Nome = "henrique",
+                Divida = "100$",
+                id = "216914"           
+            });
+        }
+        public IActionResult Index()
+        {
+            return View(devendo);
+        }
+
+        public IActionResult Details(string id)
+        {
+            var endividado = devendo.Find(a => a.id == id);
+            if (endividado == null) return NotFound();
+            return View(endividado);
+        }
+    }
+}
---
+++ MVC/Models/Serasa.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC.Models
+{
+    public class Serasa
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string id { get; set; } = string.Empty;
+        public string Divida { get; set; } = string.Empty;
+
+    }
+}
